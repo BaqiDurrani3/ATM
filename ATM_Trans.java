7d825ec156c0da92862df8d6a4d9220a4a4cd555
DurraniBaqi3 <durrani.baqi1@gmail.com>
2017-11-24 04:56:12 +0430

    first file is added

---
+++ ATM_Trans.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class ATM_Trans{
+    public static void main(String[] args){
+        int balance=6000, withdraw, deposit;
+        Scanner input=new Scanner(System.in);
+        while(true){
+            System.out.println("Welcome to ATM (Atumated Teller Machin)");
+            System.out.println("press 1 For withdraw");
+            System.out.println("press 2 for deposit");
+            System.out.println("press 3 for Checking Balance");
+            System.out.println("press 4 for EXIT");
+            System.out.println("Select the operation you want");
+            int n=input.nextInt();
+            
+        }
+    }
+    
+}
